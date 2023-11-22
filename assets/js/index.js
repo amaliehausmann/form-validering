0a12d8dddb8432cde8d92f6032bc4ebc1308fe65
amaliehausmann <amaliehausmann@gmail.com>
2023-11-22 14:50:13 +0100

    form

---
+++ assets/js/index.js
@@ -0,0 +1,138 @@
+const formParent = document.getElementById('myForm');
+const formContain = document.getElementById('formContainer');
+
+const myFirstName = document.getElementById('firstName');
+const firstNameError = document.getElementById('firstNameError');
+
+const myLastName = document.getElementById('lastName');
+const lastNameError = document.getElementById('lastNameError');
+
+const myAdress = document.getElementById('myAdress');
+const adressError = document.getElementById('myAdressError');
+
+const myPostalNumber = document.getElementById('postalNumber');
+const postalError = document.getElementById('postalNumberError');
+
+const myEmail = document.getElementById('myEmail');
+const emailError = document.getElementById('myEmailError');
+
+
+const myButton = document.getElementById('submitButton');
+
+
+myForm.addEventListener('input', (event) => {
+    event.preventDefault();
+
+    if (myFirstName.value.trim().length > 1) {
+        console.log('Fornavnet er langt nok');
+        firstNameError.textContent = '';
+        myFirstName.classList.remove('invalid');
+        myFirstName.classList.add('valid');
+        firstNameError.style.display = 'none';
+    } else{
+        console.log('Fornavnet er IKKE langt nok');
+        firstNameError.textContent = 'Fornavn skal udfyldes!';
+        myFirstName.classList.remove('valid');
+        myFirstName.classList.add('invalid');
+        firstNameError.style.display = 'block';
+    }
+
+    if (myLastName.value.trim().length > 1){
+        console.log('Efternavnet er langt nok');
+        lastNameError.textContent = '';
+        myLastName.classList.remove('invalid');
+        myLastName.classList.add('valid');
+        lastNameError.style.display = 'none';
+    } else{
+        console.log('Efternavnet er ÍKKE langt nok');
+        lastNameError.textContent = 'Efternavn skal udfyldes!';
+        myLastName.classList.remove('valid');
+        myLastName.classList.add('invalid');
+        lastNameError.style.display = 'block';
+    }
+
+    if (myAdress.value.trim().length > 5){
+        console.log('Adressen er lang nok');
+        adressError.textContent = '';
+        myAdress.classList.remove('invalid');
+        myAdress.classList.add('valid');
+        adressError.style.display = 'none';
+    } else{
+        console.log('Adressen er IKKE lang nok');
+        myAdress.classList.toggle('errorMarking');
+        adressError.textContent = 'Adresse skal udfyldes!';
+        myAdress.classList.remove('valid');
+        myAdress.classList.add('invalid');
+        adressError.style.display = 'block';
+    }
+
+    if (postalValidation(myPostalNumber.value)){
+        console.log('Dette er et gyldigt postnummer');
+        postalError.textContent = '';
+        myPostalNumber.classList.remove('invalid');
+        myPostalNumber.classList.add('valid');
+        postalError.style.display = 'none';
+    }  else{
+        console.log('Dette er IKKE et gyldigt postnummer');
+        postalError.textContent = 'Postnummer skal udfyldes!';
+        myPostalNumber.classList.remove('valid');
+        myPostalNumber.classList.add('invalid');
+        postalError.style.display = 'block';
+    }
+
+    if(emailValidation(myEmail.value)){
+        console.log('Dette er en gyldig email')
+        emailError.textContent = '';
+        myEmail.classList.remove('invalid');
+        myEmail.classList.add('valid');
+        emailError.style.display = 'none';
+    } else{
+        console.log('Dette er IKKE en gyldig email');
+        emailError.textContent = 'Email skal udfyldes!';
+        myEmail.classList.remove('valid');
+        myEmail.classList.add('invalid');
+        emailError.style.display = 'block';
+    }
+
+});
+
+myButton.addEventListener('click', (event) =>{
+    event.preventDefault();
+
+    if(myFirstName.value.trim().length > 1 && myLastName.value.trim().length > 1 && myAdress.value.trim().length > 5 && postalValidation(myPostalNumber.value) && emailValidation(myEmail.value)){
+        console.log('Formen er udfyldt korrekt');
+
+        let myResponseElement = document.createElement('h2');
+        myResponseElement.innerText = 'Tak for din tilmelding!';
+        
+        formContain.appendChild(myResponseElement);
+
+        formParent.style.display = 'none';
+
+        myResponseElement.style.display = 'block';
+        myResponseElement.style.textAlign = 'center';
+        myResponseElement.style.marginTop = '20%';
+        myResponseElement.style.fontFamily = 'sans-serif'
+        myResponseElement.style.color = 'white';
+        myResponseElement.style.fontSize = '4vw';
+    } else{
+        alert('Formularen skal udfyldes!')
+    }
+
+})
+
+function postalValidation(postnummer){
+    let postalRegex = /^\d+$/;
+
+    let isValid = postalRegex.test(postnummer);
+
+    return isValid;
+}
+
+function emailValidation(email){
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    let isValid = emailRegex.test(email);
+
+    return isValid;
+}
